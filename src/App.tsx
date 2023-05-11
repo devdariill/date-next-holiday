b20f44b6ca7d566a888bb88d73396fed2191ca30
nATOr <gowluonade@gmail.com>
2023-05-10 22:46:37 -0500

    new Date ( new Date year . . . )

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 import './App.css'
+import { HOLIDAYS } from './mocks/Holidays'
+
+const today = new Date()
+const nextHoliday = HOLIDAYS.find(holiday => new Date(holiday.date) > today) || {...HOLIDAYS[0],date: new Date(
+  new Date(today).getFullYear(),
+  new Date(HOLIDAYS[0].date).getMonth(),
+  new Date(HOLIDAYS[0].date).getDate() + 1
+)}
+console.log(nextHoliday)
 
 function App() {
 
