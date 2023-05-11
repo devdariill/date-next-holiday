3876a76976d80a5bc1c2f241dc666b097e69518d
nATOr <gowluonade@gmail.com>
2023-05-10 22:39:18 -0500

    data

---
+++ src/mocks/Holidays.tsx
@@ -0,0 +1,78 @@
+export const HOLIDAYS = [
+    {
+        date: "2023-01-01",
+        name: "Año Nuevo"
+    },
+    {
+        date: "2023-01-09",
+        name: "Día de los Reyes Magos"
+    },
+    {
+        date: "2023-03-20",
+        name: "Día de San José"
+    },
+    {
+        date: "2023-04-09",
+        name: "Domingo de Ramos"
+    },
+    {
+        date: "2023-04-14",
+        name: "Viernes Santo"
+    },
+    {
+        date: "2023-04-16",
+        name: "Domingo de Resurrección"
+    },
+    {
+        date: "2023-05-01",
+        name: "Día del Trabajo"
+    },
+    {
+        date: "2023-05-29",
+        name: "Día de la Ascensión"
+    },
+    {
+        date: "2023-06-19",
+        name: "Corpus Christi"
+    },
+    {
+        date: "2023-06-26",
+        name: "Sagrado Corazón"
+    },
+    {
+        date: "2023-07-03",
+        name: "San Pedro y San Pablo"
+    },
+    {
+        date: "2023-07-20",
+        name: "Día de la Independencia"
+    },
+    {
+        date: "2023-08-07",
+        name: "Batalla de Boyacá"
+    },
+    {
+        date: "2023-08-21",
+        name: "Asunción de la Virgen"
+    },
+    {
+        date: "2023-10-16",
+        name: "Día de la Raza"
+    },
+    {
+        date: "2023-11-06",
+        name: "Día de Todos los Santos"
+    },
+    {
+        date: "2023-11-13",
+        name: "Independencia de Cartagena"
+    },
+    {
+        date: "2023-12-08",
+        name: "Día de la Inmaculada Concepción"
+    },
+    {
+        date: "2023-12-25",
+        name: "Navidad"
+    }
+]
